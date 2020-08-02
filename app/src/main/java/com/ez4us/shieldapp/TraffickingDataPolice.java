0e7b6b9e8f7fb02b0579f0ee0b16900fcac773bd
Bhajan <kandaribhajan@gmail.com>
2020-08-02 16:47:04 +0530

    Add Bottom Nav Menu For Authority Login

--- app/src/main/java/com/ez4us/shieldapp/TraffickingDataPolice.java
+++ app/src/main/java/com/ez4us/shieldapp/TraffickingDataPolice.java
@@ -6,43 +6,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class PoliceMainActivity extends AppCompatActivity {
+public class TraffickingDataPolice extends AppCompatActivity {
 
-    Button trafik,domestic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_police_main);
+        setContentView(R.layout.activity_trafficking_data_police);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavPol);
-        bottomNavigationView.setSelectedItemId(R.id.harVolBtn);
+        bottomNavigationView.setSelectedItemId(R.id.trafBtn);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 switch (item.getItemId()) {
-
-
                     case R.id.trafBtn:
-                        Intent intent1 = new Intent(getApplicationContext(), TraffickingDataPolice.class);
+                        break;
+
+                    case R.id.harVolBtn:
+                        Intent intent1 = new Intent(getApplicationContext(), PoliceLoginActivity.class);
                         intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(intent1);
                         overridePendingTransition(0, 0);
                         break;
 
-                    case R.id.harVolBtn:
-                        break;
 
                 }
                 return false;
 
             }
         });
-
     }
 }
